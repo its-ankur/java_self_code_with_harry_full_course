3a1604d8667aafd0be01a0aa51590a87c0e11671
its-ankur <ankuratul2007@gmail.com>
2023-12-20 19:38:33 +0530

    Practice

---
+++ src/ArrayPractice/Question1.java
@@ -0,0 +1,12 @@
+package ArrayPractice;
+
+public class Question1 {
+    public static void main(String[] args) {
+        float[] marks={45.3f,67.8f,63.4f,99.2f,100.0f};
+        float sum=0;
+        for(float element:marks){
+            sum=sum+element;
+        }
+        System.out.println(sum);
+    }
+}
